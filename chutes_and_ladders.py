a3f5a08ff6786964fca4131f7a28bb3f2354941e
William Jackson <william@subtlecoolness.com>
2016-02-27 09:07:03 -0600

    Remove Board class

--- chutes_and_ladders.py
+++ chutes_and_ladders.py
@@ -5,18 +5,14 @@ def log(message):
     print(message)
 
 
-class Board(object):
+def get_next_space(start, roll):
     special = {1: 38, 4: 14, 9: 31, 16: 6, 21: 42, 28: 84, 36: 44, 48: 26,
                49: 11, 51: 67, 56: 53, 62: 19, 64: 60, 71: 91, 80: 100, 87: 24,
                93: 73, 95: 75, 98: 78}
-
-    def get_next_space(self, start, roll):
-        next_space = start + roll
-        if next_space > 100:
-            return start
-        if next_space in self.special:
-            return self.special.get(next_space)
-        return next_space
+    next_space = start + roll
+    if next_space > 100:
+        return start
+    return special.get(next_space, next_space)
 
 
 class Player(object):
@@ -34,18 +30,17 @@ class Player(object):
 def main():
     game_on = True
     round_num = 0
-    board = Board()
     players = []
     for name in ['Player 1', 'Player 2', 'Player 3', 'Player 4']:
         players.append(Player(name))
     log('** Let\'s play Chutes and Ladders!')
     while game_on:
-        round_num = round_num + 1
+        round_num += 1
         log('** Begin round {}'.format(round_num))
         for player in players:
             roll = random.randint(1, 6)
             old_space = player.space
-            player.space = board.get_next_space(player.space, roll)
+            player.space = get_next_space(player.space, roll)
             m = '{} rolled {}, moved from {} to {}'
             log(m.format(player, roll, old_space, player.space))
             if player.space == 100:
